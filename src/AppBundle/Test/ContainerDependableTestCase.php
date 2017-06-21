c1b95a5b50b52b84e5d1bf5ea08e5433b3ba36c3
Evis Bregu <evis.bregu@gmail.com>
2017-06-21 15:40:29 +0200

    Added testcase with symfony container

--- src/AppBundle/Test/ContainerDependableTestCase.php
+++ src/AppBundle/Test/ContainerDependableTestCase.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Test;
 
 
+use AppBundle\Entity\Menu;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\DependencyInjection\Container;
 
@@ -25,5 +26,21 @@ class ContainerDependableTestCase extends KernelTestCase
     protected function get($service)
     {
         return $this->_container->get($service);
+    }
+
+    public function createDummyMenuEntries($count = 20)
+    {
+        for ($i = 0; $i < $count; $i++) {
+            $menu = new Menu();
+            $menu->setNavHeader("")
+                ->setIcon("glyphicon glyphicon-th-list")
+                ->setName("Menu".$i)
+                ->setLink("menu_".$i)
+                ->setParentId(0)
+                ->setSort(1)
+                ->setStatus(true);
+            $this->_container->get("doctrine")->getManager()->persist($menu);
+        }
+        $this->_container->get("doctrine")->getManager()->flush();
     }
 }
