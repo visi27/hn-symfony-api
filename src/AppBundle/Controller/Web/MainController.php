dadc0be1747e7d5c326fb6bda88d9c079888bf64
Evis Bregu <evis.bregu@gmail.com>
2017-06-30 10:18:46 +0200

    New homepage, rendering README.md

--- src/AppBundle/Controller/Web/MainController.php
+++ src/AppBundle/Controller/Web/MainController.php
@@ -8,14 +8,23 @@
 
 namespace AppBundle\Controller\Web;
 
+use Highlight\Highlighter;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class MainController extends Controller
 {
     public function homepageAction()
     {
-        $this->addFlash('success', 'System Ready');
+        $hl = new Highlighter();
+        $highlighted = $hl->highlight("markdown", file_get_contents('../README.md'));
 
-        return $this->render('main/homepage.html.twig');
+        $content = $this->stripFirstLine($this->stripFirstLine($highlighted->value));
+
+        return $this->render('main/homepage.html.twig', ["content" => $content, "language"=>$highlighted->language]);
+    }
+
+    private function stripFirstLine($text)
+    {
+        return substr( $text, strpos($text, "\n")+1 );
     }
 }
