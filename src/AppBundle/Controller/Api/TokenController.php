0503b9e8f15f8ea297fec0329e611407d2692f0c
Evis Bregu <evis.bregu@gmail.com>
2018-05-08 16:20:51 +0200

    Added user details route

--- src/AppBundle/Controller/Api/TokenController.php
+++ src/AppBundle/Controller/Api/TokenController.php
@@ -7,12 +7,15 @@
 namespace AppBundle\Controller\Api;
 
 use Lexik\Bundle\JWTAuthenticationBundle\Encoder\JWTEncoderInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Exception\JWTDecodeFailureException;
+use Lexik\Bundle\JWTAuthenticationBundle\TokenExtractor\AuthorizationHeaderTokenExtractor;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Core\Exception\BadCredentialsException;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
 
 class TokenController extends BaseController
 {
@@ -48,10 +51,41 @@ class TokenController extends BaseController
         $token = $JWTEncoder->encode(
             [
                 'username' => $user->getUsername(),
+                'id' => $user->getId(),
                 'exp' => time() + 3600, // 1 hour expiration
             ]
         );
 
         return new JsonResponse(['token' => $token]);
+    }
+
+    /**
+     * @Route("/api/user", name="api_user_details")
+     * @Method("GET")
+     *
+     * @param Request $request
+     *
+     * @param JWTEncoderInterface $JWTEncoder
+     *
+     * @return JsonResponse
+     * @throws \Lexik\Bundle\JWTAuthenticationBundle\Exception\JWTEncodeFailureException
+     */
+    public function getUserFromToken(Request $request, JWTEncoderInterface $JWTEncoder) {
+        $extractor = new AuthorizationHeaderTokenExtractor(
+            'Bearer',
+            'Authorization'
+        );
+        $token = $extractor->extract($request);
+
+        try {
+            $data = $JWTEncoder->decode($token);
+        } catch (JWTDecodeFailureException $e) {
+            // if you want to, use can use $e->getReason() to find out which of the 3 possible things went wrong
+            // and tweak the message accordingly
+            // https://github.com/lexik/LexikJWTAuthenticationBundle/blob/05e15967f4dab94c8a75b275692d928a2fbf6d18/Exception/JWTDecodeFailureException.php
+            throw new CustomUserMessageAuthenticationException('Invalid Token');
+        }
+
+        return new JsonResponse($data);
     }
 }
