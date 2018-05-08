37869d9a32814d83ebaef44fbb807e8ac78be48e
Evis Bregu <evis.bregu@gmail.com>
2018-05-08 16:20:33 +0200

    Add CorsListener which subscribes to kernel events and handle OPTIONS preflight requests

---
+++ src/AppBundle/EventListener/CorsListener.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Created by Evis Bregu <evis.bregu@gmail.com>.
+ * Date: 3/29/18
+ * Time: 11:05 AM
+ */
+
+namespace AppBundle\EventListener;
+
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class CorsListener implements EventSubscriberInterface
+{
+
+    /**
+     * Returns an array of event names this subscriber wants to listen to.
+     *
+     * The array keys are event names and the value can be:
+     *
+     *  * The method name to call (priority defaults to 0)
+     *  * An array composed of the method name to call and the priority
+     *  * An array of arrays composed of the method names to call and respective
+     *    priorities, or 0 if unset
+     *
+     * For instance:
+     *
+     *  * array('eventName' => 'methodName')
+     *  * array('eventName' => array('methodName', $priority))
+     *  * array('eventName' => array(array('methodName1', $priority), array('methodName2')))
+     *
+     * @return array The event names to listen to
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::REQUEST => array('onKernelRequest', 9999),
+            KernelEvents::RESPONSE => array('onKernelResponse', 9999),
+        ];
+    }
+
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        // Don't do anything if it's not the master request.
+        if (!$event->isMasterRequest()) {
+            return;
+        }
+        $request = $event->getRequest();
+        $method  = $request->getRealMethod();
+        if ('OPTIONS' == $method) {
+            $response = new Response();
+            $event->setResponse($response);
+        }
+    }
+
+    public function onKernelResponse(FilterResponseEvent $event)
+    {
+        // Don't do anything if it's not the master request.
+        if (!$event->isMasterRequest()) {
+            return;
+        }
+        $response = $event->getResponse();
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET,POST,PUT');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    }
+}
