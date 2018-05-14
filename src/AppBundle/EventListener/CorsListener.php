77e2a2a264c82517d2e12a93128d627a8d8965ce
Evis Bregu <evis.bregu@gmail.com>
2018-05-14 17:19:18 +0200

    Added DELETE to allowed methods

--- src/AppBundle/EventListener/CorsListener.php
+++ src/AppBundle/EventListener/CorsListener.php
@@ -65,7 +65,7 @@ class CorsListener implements EventSubscriberInterface
         }
         $response = $event->getResponse();
         $response->headers->set('Access-Control-Allow-Origin', '*');
-        $response->headers->set('Access-Control-Allow-Methods', 'GET,POST,PUT');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     }
 }
