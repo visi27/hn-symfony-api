d22d31e9b816c5dc85008fd9603592275c10c14b
Evis Bregu <evis.bregu@gmail.com>
2018-05-16 16:38:14 +0200

    Removed code being there by mistake (tests)

--- src/AppBundle/Controller/Api/V1/HomeController.php
+++ src/AppBundle/Controller/Api/V1/HomeController.php
@@ -28,35 +28,3 @@ class HomeController extends BaseController
         return $response;
     }
 }
-
-
-
-
-
-
-
-// Create the tcp client
-$client = new swoole_client(SWOOLE_SOCK_TCP);
-
-// Connect to the tcp server
-if(!$client->connect('127.0.0.1', 9501, 0.5))
-{
-    die("connect failed");
-}
-
-// Send data to the tcp server
-if(!$client->send("Hello World"))
-{
-    die("send failed");
-}
-
-// Receive data from the tcp server
-$data = $client->recv();
-if(!$data)
-{
-    die("recv failed");
-}
-echo $data;
-
-// Close the connection
-$client->close();
